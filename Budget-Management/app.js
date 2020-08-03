b0b78ad51c04ddb3a2617ec6bc82360f5f38177f
Miraziz <khidoyatov1997@gmail.com>
2020-08-03 20:29:19 +0200

    added ui functionalities

--- Budget-Management/app.js
+++ Budget-Management/app.js
@@ -162,18 +162,19 @@ const BudgetController = (() => {
 
 const UIController = (() => {
   const DOMstrings = {
-    totalBudget: '.budget__value',
-    income: '.budget__income--value',
-    expense: '.budget__expenses--value',
     inputType: '.add__type',
     inputDescription: '.add__description',
     inputValue: '.add__value',
     inputBtn: '.add__btn',
     incomeContainer: '.income__list',
-    expenseContainer: '.expense__list',
-    dateLabel: '.budget__title--month',
+    expensesContainer: '.expenses__list',
+    budgetLabel: '.budget__value',
+    incomeLabel: '.budget__income--value',
+    expensesLabel: '.budget__expenses--value',
     percentageLabel: '.budget__expenses--percentage',
-    itemPercentage: '.item__percentage',
+    container: '.container',
+    expensesPercLabel: '.item__percentage',
+    dateLabel: '.budget__title--month',
   };
 
   const formatNumber = (num, type) => {
@@ -210,8 +211,160 @@ const UIController = (() => {
   };
 
   return {
-    DOMstrings,
-  };
+    getInput: () => {
+        return {
+            type: document.querySelector(DOMstrings.inputType).value,
+            description: document.querySelector(DOMstrings.inputDescription).value,
+            value: document.querySelector(DOMstrings.inputValue).value
+        };
+    },
+
+    addListItem: (obj, type) => {
+        let html, newHtml, element
+
+        // create html string with placeholder
+        if(type === 'ince'){
+            element = DOMstrings.incomeContainer;
+
+            html =
+                '<div class="item clearfix" id="inc-%id%"> <div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+        } else if (type === 'exp') {
+            element = DOMstrings.expensesContainer;
+
+            html =
+                '<div class="item clearfix" id="exp-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+        }
+
+        // Replace the placeholder text with some actual data
+        newHtml = html.replace('%id%', obj.id);
+        newHtml = newHtml.replace('%description%', obj.description);
+        newHtml = newHtml.replace('%value%', formatNumber(obj.value, type));
+
+        // Insert the HTML into the DOM
+        document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+    },
+
+    deleteListItem: (selectorId) => {
+        let el = document.getElementById(selectorId);
+        el.parentNode.removeChild(el);
+    },
+
+    clearFileds: () => {
+        let fields, fieldsArr;
+
+        fields = document.querySelectorAll(
+            DOMstrings.inputDescription + ', ' + DOMstrings.inputValue
+        );
+
+        fieldsArr = Array.prototype.slice.call(fields);
+
+        fieldsArr.forEach(function (current, index, array) {
+            current.value = '';
+        });
+
+        fieldsArr[0].focus();
+    },
+
+    displayBudget: (obj) => {
+        let type;
+        obj.budget > 0 ? (type = 'inc') : (type = 'exp');
+
+        document.querySelector(DOMstrings.budgetLabel).textContent = formatNumber(
+            obj.budget,
+            type
+        );
+        document.querySelector(DOMstrings.incomeLabel).textContent = formatNumber(
+            obj.totalInc,
+            'inc'
+        );
+        document.querySelector(
+            DOMstrings.expensesLabel
+        ).textContent = formatNumber(obj.totalExp, 'exp');
+
+        if (obj.percentage > 0) {
+            document.querySelector(DOMstrings.percentageLabel).textContent =
+                obj.percentage + '%';
+        } else {
+            document.querySelector(DOMstrings.percentageLabel).textContent = '---';
+        }
+    },
+
+    displayPercentages: (percentages) => {
+        let fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
+
+        nodeListForEach(fields, function (current, index) {
+            if (percentages[index] > 0) {
+                current.textContent = percentages[index] + '%';
+            } else {
+                current.textContent = '---';
+            }
+        });
+    },
+
+    displayMonth: () => {
+        let now, months, month, year;
+
+        now = new Date();
+        //let christmas = new Date(2016, 11, 25);
+
+        months = [
+            'January',
+            'February',
+            'March',
+            'April',
+            'May',
+            'June',
+            'July',
+            'August',
+            'September',
+            'October',
+            'November',
+            'December',
+        ];
+        month = now.getMonth();
+
+        year = now.getFullYear();
+        document.querySelector(DOMstrings.dateLabel).textContent =
+            months[month] + ' ' + year;
+    },
+
+    changedType: () => {
+        let fields = document.querySelectorAll(
+            DOMstrings.inputType +
+            ',' +
+            DOMstrings.inputDescription +
+            ',' +
+            DOMstrings.inputValue
+        );
+
+        nodeListForEach(fields, function (cur) {
+            cur.classList.toggle('red-focus');
+        });
+
+        document.querySelector(DOMstrings.inputBtn).classList.toggle('red');
+    },
+
+    getDOMstrings: () => {
+        return DOMstrings;
+    }
+    
+ }  
+    
 })();
 
-const Controller = ((UIctrl) => {})(UIController);
+const Controller = ((UICtrl, BdgCtrl) => {
+    const setupEventListeners = () => {
+        const DOM = UICtrl.getDOMstrings();
+
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+        document.addEventListener('keypress', (even) => {
+            if(event.keyCode === 13 || even.which === 13){
+                ctrlAddItem();
+            }
+        });
+
+        document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
+        document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changedType);
+    }
+})(UIController, BudgetController);
