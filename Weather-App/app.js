da61a13250ea023fdd18f398f3c638b02e910974
Miraziz Khidoyatov <49793609+Mirazyzz@users.noreply.github.com>
2020-10-05 17:43:26 +0200

    changed api key for weather fetching

--- Weather-App/app.js
+++ Weather-App/app.js
@@ -153,3 +153,23 @@ function dateBuilder(d) {
 
   return `${day} ${date} ${month} ${year}`;
 }
+
+async function getWeatherInfo(loc){
+  fetch("https://community-open-weather-map.p.rapidapi.com/weather?callback=test&id=2172797&units=%2522metric%2522%20or%20%2522imperial%2522&mode=xml%252C%20html&q=London%252Cuk", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
+		"x-rapidapi-key": "SIGN-UP-FOR-KEY"
+	  }
+  })
+  .then(response => {
+    console.log(response);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+const p = 0;
+};
+
+getWeatherInfo('Warsaw');
