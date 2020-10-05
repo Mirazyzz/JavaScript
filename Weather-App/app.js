f0937a01bbd360028b5a1ad274f19a596c97b468
Miraziz Khidoyatov <49793609+Mirazyzz@users.noreply.github.com>
2020-10-05 18:47:24 +0200

    added weather info

--- Weather-App/app.js
+++ Weather-App/app.js
@@ -154,22 +154,18 @@ function dateBuilder(d) {
   return `${day} ${date} ${month} ${year}`;
 }
 
-async function getWeatherInfo(loc){
-  fetch("https://community-open-weather-map.p.rapidapi.com/weather?callback=test&id=2172797&units=%2522metric%2522%20or%20%2522imperial%2522&mode=xml%252C%20html&q=London%252Cuk", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
-		"x-rapidapi-key": "SIGN-UP-FOR-KEY"
-	  }
-  })
-  .then(response => {
-    console.log(response);
-  })
-  .catch(err => {
-    console.log(err);
-  });
+async function getWeatherInfo(loc) {
+  try {
+    const result = await fetch(
+      `https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/search/?query=${loc}`
+    );
 
-const p = 0;
-};
+    const data = await result.json();
+
+    return data[0].woeid;
+  } catch (err) {
+    console.log(err);
+  }
+}
 
 getWeatherInfo('Warsaw');
