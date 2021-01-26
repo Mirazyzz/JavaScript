8b9ae6edaf4d852982d74ab8136ce4a419f0f4f4
Miraziz Khidoyatov <49793609+Mirazyzz@users.noreply.github.com>
2021-01-26 20:40:12 +0100

    added inital content and global variables

--- Dice-Game/script.js
+++ Dice-Game/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+// global variables
+const firstScore = document.getElementById('score--0');
+const secondScore = document.getElementById('score--1');
+const diceElement = document.querySelector('.dice');
+
+firstScore.textContent = '0';
+secondScore.textContent = '0';
+diceElement.classList.add('hidden');
