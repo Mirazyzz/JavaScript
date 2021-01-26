2578a1bfc526582b4d7c07fcb032f19acf764842
Miraziz Khidoyatov <49793609+Mirazyzz@users.noreply.github.com>
2021-01-26 22:07:26 +0100

    added set up modal form

--- Dice-Game/script.js
+++ Dice-Game/script.js
@@ -4,7 +4,47 @@
 const firstScore = document.getElementById('score-0');
 const secondScore = document.getElementById('score-1');
 const diceElement = document.querySelector('.dice');
+const modalSetup = document.querySelector('.modal');
 
-firstScore.textContent = '0';
-secondScore.textContent = '0';
-diceElement.classList.add('hidden');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+const btnNewGame = document.querySelector('.btn-new');
+
+// setting up event listeners
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+btnNewGame.addEventListener('click', () => openModal());
+
+// document.addEventListener('keydown', function (e) {
+//   // console.log(e.key);
+
+//   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+//     closeModal();
+//   }
+// });
+
+// firstScore.textContent = '0';
+// secondScore.textContent = '0';
+// diceElement.classList.add('hidden');
+
+// const openModal = () => {
+//   modal.classList.remove('hidden');
+//   overlay.classList.remove('hidden');
+// };
+
+// const closeModal = () => {
+//   modal.classList.add('hidden');
+//   overlay.classList.add('hidden');
+// };
+
+function openModal() {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+}
+
+function closeModal() {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
