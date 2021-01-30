e8154b2a2acf03b37fb522c408d6e18aaf648825
Miraziz Khidoyatov <49793609+Mirazyzz@users.noreply.github.com>
2021-01-30 23:22:46 +0100

    added setup initializer and validation on modal

--- Dice-Game/script.js
+++ Dice-Game/script.js
@@ -3,6 +3,8 @@
 // global variables
 const firstScore = document.getElementById('score-0');
 const secondScore = document.getElementById('score-1');
+const firstPlayer = document.querySelector('.player-0');
+const secondPlayer = document.querySelector('.player-1');
 const diceElement = document.querySelector('.dice');
 const modalSetup = document.querySelector('.modal');
 
@@ -11,6 +13,8 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 const btnNewGame = document.querySelector('.btn-new');
+
+let goalScore;
 
 // setting up event listeners
 btnCloseModal.addEventListener('click', closeModal);
@@ -45,6 +49,22 @@ function openModal() {
 }
 
 function closeModal() {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
+  const goalScore = document.getElementById('goal-score');
+
+  if (goalScore.value) {
+    goalScore.style.border = '2px solid';
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+
+    setupGame(goalScore.value);
+  } else {
+    document.getElementById('goal-score').style.borderColor = 'red';
+    alert('Please, enter goal score value');
+  }
 }
+
+const setupGame = (goalScore) => {
+  goalScore = goalScore;
+  firstScore.textContent = '0';
+  secondScore.textContent = '0';
+};
